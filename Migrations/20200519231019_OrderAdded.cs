e8d8f4b2298e934ffb03310b971c9409ffd2356e
Miriam Gomez <gira53_5@hotmail.com>
2022-09-11 13:14:48 -0500

    Final Code

---
+++ Migrations/20200519231019_OrderAdded.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class OrderAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    NoProductos = table.Column<int>(nullable: false),
+                    TotalAccount = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ Models/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public OrderRepository( AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+       public void AddOrder(Order order)
+        {
+            _appDbContext.Orders.Add(order);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
